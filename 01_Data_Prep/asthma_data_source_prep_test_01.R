f0a904a1c0beaead895aa6fe23f0d3f96c7253ff
drbulu <bulukani.mlalazi@gmail.com>
2017-02-09 00:19:13 +1000

    asthma data processing - yet more work to tweak and standardise data consolidation.

--- 01_Data_Prep/asthma_data_source_prep_test_01.R
+++ 01_Data_Prep/asthma_data_source_prep_test_01.R
@@ -41,7 +41,7 @@ cleanBasicNames = function(targetTable, isGenderPercent=T){
     names(targetTable) = gsub("Prevalence(.)+[Pp]ercent(.)+", "Prev.perc", names(targetTable))
     names(targetTable) = gsub("Prevalence(.)+[Nn]umber(.)+", "Prev.num", names(targetTable))
     names(targetTable) = gsub("StandardError", "Prev.perc.SE", names(targetTable))
-    names(targetTable) = gsub("RaceandEthnicity", "Ethnicity", names(targetTable))
+    names(targetTable) = gsub("[Rr]ace(.)+[Ee]thnicity", "Ethnicity", names(targetTable))
     if(isGenderPercent){
         names(targetTable) = gsub("FemalePrevalence", "Female.Prev.perc", names(targetTable))
         names(targetTable) = gsub("MalePrevalence", "Male.Prev.perc", names(targetTable))        
@@ -94,18 +94,29 @@ adultOverallTablesList = getDatasetTablesFromURL(asthma_URL_table_list$adult.Ove
 # cleanAdultOverallTablesList = lapply(adultOverallTablesList, FUN = cleanAsthmaTableNames)
 # c) merge preppped tables - new function required
 
+# lovely little function - uses REGEX to cut out matching text: 
+# inspired by searching ?regmatches (help page) on RStudio console
+
+extractMatch = function(x, pattern, invertMatch=F){
+    result = regmatches(x, regexpr(pattern, x), invert = invertMatch)
+    return(result)
+}
+
 # Function to clean the names of a list of related tables: 
 # e.g. the same data set collected over different years
 # if 95% CI data is needed in merged data, there needs
 # to be a way of preprocessing the names to make them
 # unique. Not presently worth the time. No use case defined!
 
-createDataseriesFromList = function(sourceTableList){
+createDataseriesFromList = function(sourceTableList, sortData = T){
     # clean the names of the data.frames in sourceTableList
     sourceTableList = lapply(sourceTableList, FUN = cleanAsthmaTableNames)
     # can add table name cleaning step here
     for(i in 1:length(sourceTableList)){
-        sourceTableList[[i]][, "Table.ID"] = names(sourceTableList)[[i]]
+        tableID = names(sourceTableList)[[i]]
+        sourceTableList[[i]][, "Group.ID"] = extractMatch(tolower(tableID), "adult|child")
+        sourceTableList[[i]][, "Year"] = extractMatch(tableID, "[0-9]{4}")
+        sourceTableList[[i]][, "Table.ID"] = extractMatch(tableID, "[C|L][0-9]+")
     }    
     # need to get rid of the 95% CI columns: Neither needed nor unique!
     sourceTableList = lapply(sourceTableList, FUN = function(x){
@@ -113,11 +124,61 @@ createDataseriesFromList = function(sourceTableList){
         return(x[, colsToKeep])
     })
     # Seamlessly condense dataframe list: http://stackoverflow.com/questions/8091303/
-    mergedTable = Reduce(function(...) merge(..., all=T), sourceTableList)    
+    mergedTable = Reduce(function(...) merge(..., all=T), sourceTableList)
+    # For conveience: order table by Table.ID, then by State, then by Year    
+    if(sortData) mergedTable = mergedTable[order(
+        mergedTable$Table.ID, 
+        mergedTable$State, 
+        mergedTable$Year), ]
     return(mergedTable)
 }
 
 # tested it using the Adult data series
-adultOverallDataSeries = createDataseriesFromList(cleanAdultOverallTablesList)
+adultOverallTablesList = getDatasetTablesFromURL(asthma_URL_table_list$adult.Overall)
+adultOverallDataSeries = createDataseriesFromList(adultOverallTablesList)
 
+## basically, the two essential elements are
+# 1. getDatasetTablesFromURL(asthma_URL_metadata_table)
+# 2. createDataseriesFromList(asthma_URL_metadata_table_list)
 
+## this needs to be extended to:
+## get lists of data FROM lists of metadata
+# 1. a = lapply(asthma_URL_metadata_table_list, FUN=getDatasetTablesFromURL)
+# 2. b = lapply(a, FUN=createDataseriesFromList)
+
+# deceptively simple function, summarising all of the above functionality
+# this pretty much completes the functionality. The next thing is to create
+# checks for data processing to date, to improve its integrity before
+# embarking on exploratory analysis!
+createDataseriesList = function(asthmaURLTableList){
+    dataTableList = lapply(asthmaURLTableList, FUN=getDatasetTablesFromURL)
+    return(lapply(dataTableList, FUN=createDataseriesFromList))
+}
+
+# complete test, to start picking up possible errors
+asthmaDataSeriesList = createDataseriesList(asthma_URL_table_list)
+
+# check function
+
+checkTableListDim = function(dataFrameList){
+    resultList = lapply(dataFrameList, FUN = function(x){
+        y = dim(x)
+        return(data.frame(row=y[1], col=y[2]))
+    })
+    return(resultList)
+}
+
+testDim = checkTableListDim(asthmaDataSeriesList)
+
+# NOTE: used the testing to tweak the regex
+# child - ethnicity (fixed regex)
+# adult - ethnic group (fixed: same issue as child)
+# adult - Gender: redundancy in multiple columns ...
+
+adultGenderDataList = getDatasetTablesFromURL(asthma_URL_table_list$adult.Gender)
+
+checkTableListNames = function(dataFrameList) return(lapply(dataFrameList, FUN = names))
+adultGenderListNames = checkTableListNames(adultGenderDataList)
+# Now we can work on File I/O a bit ... need to be able to 
+# 1. save a list of data.frames to a folder
+# 2. retrieve the data frames from a list of files or a folder into a list
