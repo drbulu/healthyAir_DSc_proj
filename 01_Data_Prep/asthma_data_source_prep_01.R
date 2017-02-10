1e692dfb3e161c9889c85975a460acd93a07f2b5
drbulu <bulukani.mlalazi@gmail.com>
2017-02-11 07:55:14 +1000

    data processing starting working on other sections

--- 01_Data_Prep/asthma_data_source_prep_01.R
+++ 01_Data_Prep/asthma_data_source_prep_01.R
@@ -44,4 +44,29 @@ asthmaDataSeriesList = preprocessAndMergeData(asthmaListofDataTableLists)
 ## 04 - Save files to disc for further analysis!
 # probably in a common data dir!
 
+# file save function: should probably be a helper, since it can
+# generally handle a simple named list of data.frames and loop
+# through said list and create a group of auto-named CSV files.
+# note: this function requires a named list
+saveDataListCsv = function(dataList, dataDir = "./", overwrite=F){
+    # create dataDir with any subdirs    
+    if (!dir.exists(dataDir)) dir.create(dataDir, recursive = T)
+    # helper to create file path
+    prepFilePath = function(n, dirName){
+        n = gsub("( )+", "_", n)
+        fileName = paste0(tolower(gsub("\\.", "_", n)), ".csv")
+        return(paste(dirName, fileName, sep="/"))
+    }
+    # write dataList elements to CSV using element name to derive filename
+    # noEcho is a lazy attempt to not echo a null list to console. write.csv()
+    # returns NULL/void.
+    noEcho = lapply(names(dataList), FUN=function(x, y){ write.csv(x = y[[x]], 
+        file = prepFilePath(x, dataDir), row.names = F) }, dataList)    
+}
+
+# writing 
+asthmaDataDir = "./Data/asthma/series/"
+
+saveDataListCsv(asthmaDataSeriesList, asthmaDataDir)
+
 rm(asthma_helper_func_02)
---
+++ 01_Data_Prep/pollution_data_source_prep_01.R
@@ -0,0 +1,5 @@
+#### Asthma Prevanence data: Data Preparation 1 ####
+
+## 01 - Download Excel file from URL
+
+trafficMainPage = "https://www.epa.gov/sites/production/files/2016-12/state_tier1_90-16.xls"
